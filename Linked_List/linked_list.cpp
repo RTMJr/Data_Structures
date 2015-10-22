b2abc76a549a494d58b7c4f0f24dda46443de878
Rich <richardt.mitchell@gmail.com>
2015-10-22 19:21:44 -0400

    updated Linked_List dir

--- Linked_List/linked_list.cpp
+++ Linked_List/linked_list.cpp
@@ -1,3 +1,5 @@
+/* Implementation file for linked_list.hpp */
+
 #include "linked_list.hpp"
 #include <iostream>
 
@@ -5,17 +7,52 @@
 
 Linked_List::Linked_List()
 {
-  head->next = NULL;
+  head = NULL;
   size = 0;
+}
+
+/* Destructor */
+
+Linked_List::~Linked_List()
+{
+  // empty
+}
+
+/* copy constructor */
+
+Linked_List::Linked_List(const Linked_List& list) : size(list.get_size())
+{
+  head = list.get_head();
+  Node* original_current = list.get_head();
+  Node* copy_current = head; 
+
+  while (original_current != NULL) {
+    copy_current->value = original_current->value;
+    copy_current->next = original_current->next;
+    original_current = original_current->next;
+    copy_current = copy_current->next;
+  }
+  
+} 
+
+void Linked_List::insert_at_head(int new_value)
+{
+  Node* new_node;  
+  new_node = new Node;
+  new_node->value = new_value;
+  new_node->next = head->next;
+  head = new_node;
 }
 
 void Linked_List::print_list(const Linked_List& list_head)
 {
   Node* current;
-  current = head->next; 
+  current = head; 
 
   while (current != NULL) {
     std::cout << current->value << std::endl; 
     current = current->next;
   }
 }
+
+
--- Linked_List/linked_list.hpp
+++ Linked_List/linked_list.hpp
@@ -12,9 +12,11 @@ class Linked_List {
   public:
     Linked_List();
     ~Linked_List();
-    Linked_List(const Linked_List& list_head);
-    void insert_node(int node_value);
-    void print_list(const Linked_List& list_head);
+    Linked_List(const Linked_List& list);
+    void insert_at_head(int new_value);
+    void print_list(const Linked_List& list);
+    int get_size() const { return size; };
+    Node* get_head() const { return head; };
   private:
     Node* head;
     int size;
