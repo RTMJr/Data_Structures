9227c8e08bbcf4727e88ebf50728ca49bafeee44
Rich <richardt.mitchell@gmail.com>
2015-10-21 21:20:50 -0400

    added default constructor and print_list function

--- Linked_List/linked_list.cpp
+++ Linked_List/linked_list.cpp
@@ -1,1 +1,21 @@
 #include "linked_list.hpp"
+#include <iostream>
+
+/* Default constructor */
+
+Linked_List::Linked_List()
+{
+  head->next = NULL;
+  size = 0;
+}
+
+void Linked_List::print_list(const Linked_List& list_head)
+{
+  Node* current;
+  current = head->next; 
+
+  while (current != NULL) {
+    std::cout << current->value << std::endl; 
+    current = current->next;
+  }
+}
--- Linked_List/linked_list.hpp
+++ Linked_List/linked_list.hpp
@@ -1,10 +1,23 @@
-
+/* API for linked list */
 
 #ifndef LINKED_LIST_HPP
 #define LINKED_LIST_HPP
 
+struct Node {
+  int value;
+  Node* next;
+};
 
+class Linked_List {
+  public:
+    Linked_List();
+    ~Linked_List();
+    Linked_List(const Linked_List& list_head);
+    void insert_node(int node_value);
+    void print_list(const Linked_List& list_head);
+  private:
+    Node* head;
+    int size;
+};
 
-
-
-#endif
+#endif // LINKED_LIST_HPP
